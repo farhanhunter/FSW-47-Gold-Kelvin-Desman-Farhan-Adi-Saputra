cce3c59e9cb2dd5b8e6be6c5a34bb22d1883a6c4
farhanhunter <96875698+farhanhunter@users.noreply.github.com>
2024-06-22 14:47:31 +0700

    error ejs

--- controllers/provinsiController.js
+++ controllers/provinsiController.js
@@ -4,28 +4,13 @@ const path = require("path");
 
 class ProvinsiController {
   getProvinsi(req, res) {
-    const provinsiList = provinsiModel.getAllProvinsi();
-    fs.readFile(
-      path.join(__dirname, "../views/provinsiView.html"),
-      (err, data) => {
-        console.log(data);
-        if (err) {
-          res.status(500).send("Error reading provinsi.json");
-          return;
-        } else {
-          const rendered = data.replace(
-            "{{provinsi}}",
-            provinsiList.map((item) => `<li>${item}</li>`).join("")
-          );
-          res.status(200).send(rendered);
-        }
-      }
-    );
+    const provinsi = provinsiModel.getAllProvinsi();
+    res.render("provinsiView", { provinsi });
   }
 
   addProvinsi(req, res) {
     const newItem = new URLSearchParams(req.body).get("provinsi");
-    provinsiModel.addProvinsi(req.body["provinsi"]);
+    provinsiModel.addProvinsi(provinsi);
     res.redirect("/provinsi");
   }
 }
